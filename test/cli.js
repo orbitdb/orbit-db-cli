12eb3dfeb48c44f2d91cd156da1ae72d8af3bfaf
haad <haad@headbanggames.com>
2017-08-23 11:44:17 +0200

    Fix bin path for CLI

--- test/cli.js
+++ test/cli.js
@@ -2,7 +2,7 @@ const path = require('path')
 const { execSync } = require('child_process')
 
 const CLI = (cmd) => {
-  return execSync(`node "${path.resolve('./src/bin')}" ${cmd}`)
+  return execSync(`node "${path.resolve(__dirname, '../src/bin')}" ${cmd}`)
 }
 
 module.exports = CLI
