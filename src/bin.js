e341c043d354acce391aca0973b425ad0f1e7a87
haad <haad@headbanggames.com>
2017-09-17 20:00:52 +0200

    Add link to quick start guide in help

--- src/bin.js
+++ src/bin.js
@@ -6,6 +6,8 @@ const logo = require('./logo')
 
 argv
   .usage(logo + '\n\nUsage: $0 <command> <database>')
+  .example('\nSee the Quick Start guide on Github: https://github.com/haadcode/orbit-db-cli#quick-start\n\nOr try running:\n$0 demo hello',
+           '')
   .commandDir('commands')
   .demand(1)
   .help('help')
