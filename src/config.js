b8958979829c30ce5ac56a6b28a0d2230e2383c8
haad <haad@headbanggames.com>
2017-12-14 22:16:56 +0100

    Clean up default config

--- src/config.js
+++ src/config.js
@@ -5,32 +5,26 @@ const path = require('path')
 const defaultOrbitDbDir = './orbitdb'
 
 const conf = {
-  start: false,
   repo: process.env.IPFS_PATH || path.join(defaultOrbitDbDir, '/ipfs'),
+  start: false,
   EXPERIMENTAL: {
     pubsub: true,
   },
   config: {
     Addresses: {
       API: '/ip4/127.0.0.1/tcp/0',
+      Gateway: '/ip4/0.0.0.0/tcp/0',
       Swarm: [
         '/ip4/0.0.0.0/tcp/0',
-        // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
+        // '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
       ],
-      Gateway: '/ip4/0.0.0.0/tcp/0',
-      // API: null,
-      // Swarm: [],
-      // Gateway: null,
     },
   },
   Discovery: {
     MDNS: {
       Enabled: true,
-      Interval: 10,
+      Interval: 1,
     },
-    webRTCStar: {
-      Enabled: false,
-    }
   },
 }
 
